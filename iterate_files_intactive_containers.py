c708ec5da7dc0cb7633dbb03e644f3d548a8c7fb
Ewa Namysł <83416086+namysl@users.noreply.github.com>
2024-12-03 14:13:16 +0100

    Update iterate_files_intactive_containers.py

--- iterate_files_intactive_containers.py
+++ iterate_files_intactive_containers.py
@@ -1,21 +1,14 @@
 import os
 
 dir = os.fsencode("/var/ansible-dir/enamysl/docker_test/images_scan/")
-no_images = []
-images = []
+content = ""
 
 for file in os.listdir(dir):
-        concatenated = dir.decode('utf-8') + file.decode('utf-8')
-        print(concatenated)
-        with open (concatenated, 'r') as opened:
-                lines = len(opened.readlines())
-                print("number of lines:", lines, "in", file)
-                if lines > 4:
-                        images.append(file)
-                else:
-                        no_images.append(file)
-
-images.sort()
-
-for item in images:
-        print(item.decode('utf-8').split('.')[0]) 
+    concatenated = dir.decode('utf-8') + file.decode('utf-8')
+    with open(concatenated, 'r') as opened:
+        content = opened.readlines()
+        if len(content) > 4:
+            with open("inactive_containers.txt", "a") as output_file:
+                for line in content:
+                    output_file.write(line)
+                output_file.write("\n\n") 
